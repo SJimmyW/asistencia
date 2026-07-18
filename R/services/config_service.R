170a1540f6cdb8734a6c7c4c67c21a2fd2bae476
SJimmyW <163605349+SJimmyW@users.noreply.github.com>
2026-07-17 21:19:30 -0300

    Create config_service.R

---
+++ R/services/config_service.R
@@ -0,0 +1,33 @@
+# ============================================================
+# asiste
+# Servicio de configuración
+# ============================================================
+
+#' Obtener configuración de la aplicación
+#' Wrapper para acceder a config.yml.
+#'
+#' @return Lista de configuración.
+#' @export
+#'
+
+get_app_config <- function(){
+
+  if( is.null( getOption("asiste.config") ) ){
+
+    cfg <- load_config()
+    options( asiste.config = cfg)
+  }
+
+  getOption( "asiste.config" ) }
+
+#' Obtener configuración Google Sheets
+#' @return configuración Google.
+#'
+#' @export
+
+get_google_config <- function(){
+
+  cfg <- get_app_config()
+
+  cfg$google
+}
