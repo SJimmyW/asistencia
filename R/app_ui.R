afde39110487176ea77c57bf7a55b73bc17f62c4
SJimmyW <163605349+SJimmyW@users.noreply.github.com>
2026-07-17 22:49:26 -0300

    Refactor app_ui function and enhance comments
    Updated documentation and improved UI structure.

--- R/app_ui.R
+++ R/app_ui.R
@@ -3,22 +3,20 @@
 # Interfaz principal
 # ============================================================
 
-#' Aplicación UI, construye la interfaz principal.
-#'
+#' Aplicación UI
+#' Construye la interfaz principal de la aplicación.
 #' @export
+#'
 
 app_ui <- function(){
+  shiny::fluidPage(
+    theme = bslib::bs_theme( version = 5, bootswatch = "flatly" ),
 
-  shiny::fluidPage( theme = bslib::bs_theme(  version = 5,
-                                            bootswatch = "flatly" ),
+    shiny::tags$head( shiny::tags$title( APP_NAME )  ),
 
-    shiny::div( class = "container",
-               shiny::h1( APP_NAME ),
+    shiny::div( class = "container-fluid",
+               shiny::uiOutput( "page" )
 
-      shiny::p( "Sistema abierto de asistencia" ),
-
-      shiny::hr(),
-
-      shiny::uiOutput( "main_content" ) )
+    )
   )
 }
