45851de235f54553f9bcf753a91450f0c1e39fb0
SJimmyW <163605349+SJimmyW@users.noreply.github.com>
2026-07-17 23:09:01 -0300

    Create tables.R

---
+++ R/components/tables.R
@@ -0,0 +1,9 @@
+# ============================================================
+# asiste
+# Tablas reutilizables
+# ============================================================
+
+data_table <- function( output_id ){
+
+  shiny::tableOutput( output_id )
+}
